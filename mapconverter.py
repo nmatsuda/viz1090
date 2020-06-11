std::list -> vector for performance. Working on mapconverter precision to merge points. Broken with lower precision levels, something wrong with QTInsert
Former-commit-id: c40d67cd1346a723dd8a3440b7eb845575c05f0b

mapconverter.py
@@ -1,6 +1,7 @@
 from lxml import etree as ElementTree
 import numpy as np
 import sys
+from tqdm import tqdm 
 
 filename = sys.argv[1]
 
@@ -12,16 +13,44 @@ parser = ElementTree.XMLParser(recover=True)
 tree = ElementTree.parse(filename, parser)
 polys = tree.xpath('//polygon')
 
-bin_file = open("mapdata.bin", "wt")
+bin_file = open("mapdata.bin", "wb")
 
 outlist = []
 
-for p in polys:
+
+precision = 2
+
+print("Reading points")
+#for i in tqdm(range(len(polys))):
+for i in range(40):
+	p = polys[i]
 	currentPoints = (p.attrib['points']).replace(","," ").split()
 
-	if(len(currentPoints) > 12): #remove little circles in the McCurley maps
-		outlist.extend(currentPoints)
+	if(len(currentPoints) == 14): #remove little circles in the McCurley maps
+		continue
+
+	prevx = 0
+	prevy = 0
+
+	temp = []
+
+	for i in range(int(len(currentPoints)/2)):
+		currentPoints[2 * i + 0] = "%.*f" % (precision, float(currentPoints[2 * i + 0]))
+		currentPoints[2 * i + 1] = "%.*f" % (precision, float(currentPoints[2 * i + 1]))
+
+		if(currentPoints[2 * i + 0] != prevx and currentPoints[2 * i + 1] != prevy):
+			temp.extend([currentPoints[2 * i + 0],currentPoints[2 * i + 1]])
+	
+		prevx = currentPoints[2 * i + 0]
+		prevy = currentPoints[2 * i + 1] 
+
+
+	if(len(currentPoints) > 14): 
+		outlist.extend(temp)
 		outlist.extend(["0","0"])
+
 
 np.asarray(outlist).astype(np.single).tofile(bin_file)
 bin_file.close()
+
+print("Wrote %d points" % (len(outlist) / 2))